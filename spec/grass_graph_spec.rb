3cf87e29ec7312a7b27ed71af011a473206f7348
rochefort <terasawan@gmail.com>
2018-11-11 12:59:03 +0900

    Remove an unused test

--- spec/grass_graph_spec.rb
+++ spec/grass_graph_spec.rb
@@ -4,8 +4,4 @@ describe GrassGraph do
   it "has a version number" do
     expect(GrassGraph::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
